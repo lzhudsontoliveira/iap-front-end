a5543e3d88d4bde9ebe34c0988b0057be896b857
Hudson Holanda <hudson.holanda@wedev.software>
2022-06-06 23:47:16 -0300

    feat: feat add Header and Footer component in all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />
+import { AppProps } from "next/app";
+import { Footer } from "../components/layout/Footer";
+import { Header } from "../components/layout/Header";
+import '../styles/global.scss'
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
