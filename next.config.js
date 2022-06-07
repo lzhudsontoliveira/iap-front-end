1f1f9014ace25b977dfabe0c434a220141a935fb
Hudson Holanda <hudson.holanda@wedev.software>
2022-06-06 23:54:01 -0300

    feat: add abstract file in all files .scss

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+    additionalData: `@import "src/styles/abstracts/index.scss";`,
+  }
 }
 
 module.exports = nextConfig
