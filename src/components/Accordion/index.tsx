6c338ea690cc84bd6398e75ce3f8f875c829a15f
Hudson Holanda <hudson.holanda@wedev.software>
2022-06-09 22:57:01 -0300

    feat(refact): change component Accordion to accept two children's

--- src/components/Accordion/index.tsx
+++ src/components/Accordion/index.tsx
@@ -1,6 +1,11 @@
-import { useRef, useState } from "react";
+import { ReactNode, useRef, useState } from "react";
 import styles from "./styles.module.scss";
-export function Accordion() {
+
+interface AccordionProps {
+  children: ReactNode;
+}
+
+export function Accordion({ children }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
   const contentEl = useRef<HTMLDivElement>(null);
   function handleToggleAccordion() {
@@ -8,29 +13,26 @@ export function Accordion() {
   }
 
   return (
-    <div className={`${styles.accordion} ${isOpen && styles['accordion--active']}`}>
+    <div
+      className={`${styles.accordion} ${isOpen && styles["accordion--active"]}`}
+    >
       <button
         onClick={handleToggleAccordion}
         type="button"
         className={styles.accordion__button}
       >
-        Quais as vantagens do IAP Cursos Online?
+        {children[0]}
       </button>
       <div
         ref={contentEl}
-        className={`${styles.accordion__content}`}
+        className={`${styles["accordion__content-container"]}`}
         style={
           isOpen
-            ? {maxHeight: contentEl.current.scrollHeight}
-            : {maxHeight: 0}
+            ? { maxHeight: contentEl.current.scrollHeight }
+            : { maxHeight: 0 }
         }
       >
-        <p className={styles.accordion__text}>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industrys standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book.
-        </p>
+        <div className={`${styles["accordion__content"]}`}>{children[1]}</div>
       </div>
     </div>
   );
