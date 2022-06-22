c9c9f4ccbdccaf2138795a372af60bdc6807fef0
Hudson Holanda <hudson@toliveira.cc>
2022-06-22 15:20:54 -0300

    feat: create Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from "react";
+import styles from "./styles.module.scss";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
+  label: string;
+}
+
+export function Input({ name, label, ...rest } : InputProps) {
+  return (
+    <div className={styles["input__wrapper"]}>
+      <label className={styles["input__label"]} htmlFor={name}>{label}</label>
+      <input {...rest} className={styles["input"]} />
+    </div>
+  );
+}
