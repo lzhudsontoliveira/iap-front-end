185a58e5de6a51e2f026f26a603483e69f02c453
Hudson Holanda <hudson@toliveira.cc>
2022-06-23 13:31:22 -0300

    feat: change to optinally property name and label and show label if label property exist

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,14 +2,18 @@ import { InputHTMLAttributes } from "react";
 import styles from "./styles.module.scss";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string;
-  label: string;
+  name?: string;
+  label?: string;
 }
 
-export function Input({ name, label, ...rest } : InputProps) {
+export function Input({ name, label, ...rest }: InputProps) {
   return (
     <div className={styles["input__wrapper"]}>
-      <label className={styles["input__label"]} htmlFor={name}>{label}</label>
+      {label && (
+        <label className={styles["input__label"]} htmlFor={name}>
+          {label}
+        </label>
+      )}
       <input {...rest} className={styles["input"]} />
     </div>
   );
