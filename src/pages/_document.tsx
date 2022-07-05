94fa963aba40f9b44fc6e0283fcba3887390ff25
Hudson Holanda <hudson@toliveira.cc>
2022-07-04 22:57:29 -0300

    fix: change url from google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,23 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-      <Head>
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&family=Roboto:ital,wght@0,400;0,300;0,500;1,700&display=swap" rel="stylesheet"></link>
-      </Head>
-      <body>
-        <Main/>
-        <NextScript/>
-      </body>
+        <Head>
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;800&family=Roboto:wght@300;400;500&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
-    )
+    );
   }
 }
